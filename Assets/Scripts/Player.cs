c558808eadc498940d7b30e5084cfacd196cd29a
ixavon1 <ixavsigler@gmail.com>
2025-02-22 17:40:33 -0500

    wepon rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("XZ Input")]
     public float speed;
     Rigidbody rb;
     Vector2 input;
 
+    [Header("Rotation")]
+    public Camera cam;
+    public Transform mousePoint, screenPoint, weaponBase, weapon;
+    private Vector2 startScreenPos;
+    float defaultWeaponOffset;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        startScreenPos = cam.WorldToScreenPoint(transform.position);
+        defaultWeaponOffset = weapon.localPosition.z;
     }
 
     private void Update()
     {
+        // XZ input
         if (Input.GetKey(KeyCode.A)) { input.x = -1; }
         else if (Input.GetKey(KeyCode.D)) { input.x = 1; }
         else { input.x = 0; }
@@ -26,7 +36,25 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Sets rb velocity
         Vector2 adjustedVelocity = input.normalized * speed;
         rb.velocity = new Vector3(adjustedVelocity.x, rb.velocity.y, adjustedVelocity.y);
+
+        // Sets rotation
+        weaponBase.eulerAngles = new(0, RotationFromMouse() + 90, 0);
+        // Offsets weapon localpos to avoid clipping through torso when weapon faces side to side
+        weapon.localPosition = new(0, 0, defaultWeaponOffset - Mathf.Abs(Mathf.Sin(weaponBase.eulerAngles.y * Mathf.Deg2Rad))/2);
+    }
+
+    private float RotationFromMouse()
+    {
+        // ROTATES TOWARDS MOUSE by projecting player onto screen and finding angle between that and mouse (its messy)
+        mousePoint.position = Input.mousePosition;
+        Vector3 screenPos = cam.WorldToScreenPoint(transform.position);
+        screenPoint.position = new Vector3(screenPos.x + Screen.width / 2 - startScreenPos.x, screenPos.y + Screen.height / 2 - startScreenPos.y, screenPos.z);
+        float rot = Mathf.Rad2Deg * Mathf.Atan2((mousePoint.position.y - screenPoint.position.y), (-mousePoint.position.x + screenPoint.position.x));
+        if (rot < 0) { rot += 360; }
+        if (rot > 360) { rot -= 360; }
+        return rot;
     }
 }
