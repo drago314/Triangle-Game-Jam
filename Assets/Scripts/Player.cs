11cf9949b47e6ddf3d12c4283e7aab4e8cafb03a
ixavon1 <ixavsigler@gmail.com>
2025-02-22 18:13:03 -0500

    wepon

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [Header("XZ Input")]
     public float speed;
+    public float sprintMod;
+    float currentSprintMod;
     Rigidbody rb;
     Vector2 input;
 
@@ -20,6 +22,8 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         startScreenPos = cam.WorldToScreenPoint(transform.position);
         defaultWeaponOffset = weapon.localPosition.z;
+
+        currentSprintMod = 1;
     }
 
     private void Update()
@@ -32,12 +36,15 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) { input.y = 1; }
         else if (Input.GetKey(KeyCode.S)) { input.y = -1; }
         else { input.y = 0; }
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) { currentSprintMod = sprintMod; }
+        else { currentSprintMod = 1; }
     }
 
     private void FixedUpdate()
     {
         // Sets rb velocity
-        Vector2 adjustedVelocity = input.normalized * speed;
+        Vector2 adjustedVelocity = input.normalized * speed * currentSprintMod;
         rb.velocity = new Vector3(adjustedVelocity.x, rb.velocity.y, adjustedVelocity.y);
 
         // Sets rotation
--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Weapon
+{
+    public float damage, fireRate, range, reloadTime;
+    public int maxClip;
+    int type;
+    public GameObject toSpawn;
+}
+
 public class PlayerWeapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int clip;
+    float reloadTimer;
+    Weapon activeWeapon;
+
+    private void Update()
     {
-        
+        if (reloadTimer > 0) { reloadTimer -= Time.deltaTime; }
+        if (Input.GetMouseButton(0)) TryFire();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TryFire()
     {
-        
+        if (reloadTimer > 0 || clip <= 0) return;
+        clip--;
+        if (clip <= 0) { clip = activeWeapon.maxClip; reloadTimer = activeWeapon.reloadTime; }
+
+
     }
 }
