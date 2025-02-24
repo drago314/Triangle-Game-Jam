cab6fbe5656d166692451bf293bb18e1d7ffd39c
Avrick Altmann <avrickaltmann@yahoo.com>
2025-02-23 23:03:50 -0500

    Make Music

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public AudioSource[] sources;
+    public float fadeTime;
+
+    int lastDimension = 0;
+    int dimension = 0;
+
+    // Use this for initialization
+    private void Start()
+    {
+        GameManager.Inst.OnDimensionSwitch += SwitchMusic;
+        dimension = (int)GameManager.Inst.dimension;
+    }
+
+    private void Update()
+    {
+        if (sources[lastDimension].volume > 0)
+            sources[lastDimension].volume -= Time.deltaTime / fadeTime;
+        if (sources[dimension].volume < 1)
+            sources[dimension].volume += Time.deltaTime / fadeTime;
+    }
+
+    private void SwitchMusic(Dimension dimension)
+    {
+        lastDimension = this.dimension;
+        this.dimension = (int)dimension;
+    }
+}
