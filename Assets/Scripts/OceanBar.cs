3a0264394c4a075e10f9e79a1ba756a3e9c77500
ixavon1 <ixavsigler@gmail.com>
2025-02-26 00:53:48 -0500

    ocean bar

--- Assets/Scripts/OceanBar.cs
+++ Assets/Scripts/OceanBar.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OceanBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public float[] values;
+    public float lerpSpeed;
+    float goal = 1;
+
+    private void Start()
     {
-        
+        GameManager.Inst.OnDimensionSwitch += SetValue;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        slider.value = Mathf.Lerp(slider.value, goal, lerpSpeed * Time.deltaTime);
+        fill.color = gradient.Evaluate(1 - slider.value);
     }
+
+    public void SetValue(Dimension dim)
+    {
+        goal = values[(int)dim];
+    }
+
 }
