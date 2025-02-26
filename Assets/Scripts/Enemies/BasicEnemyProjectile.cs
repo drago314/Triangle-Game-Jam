0485a186e01b071b2c1d73420d5bbdeb788796c8
Avrick Altmann <avrickaltmann@yahoo.com>
2025-02-26 14:06:09 -0500

    More Projectile Enemy Fixes

---
+++ Assets/Scripts/Enemies/BasicEnemyProjectile.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyProjectile : MonoBehaviour
+{
+    public int damage;
+    public float velocity, rangeInTime, knockBack;
+    public GameObject particles;
+
+    private void Start()
+    {
+        Invoke("Detonate", rangeInTime);
+    }
+
+    public void Update()
+    {
+        transform.position += transform.right.normalized * velocity * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out Player player))
+        {
+            player.GetComponent<Health>().Damage(new Damage(damage, gameObject, gameObject, knockBack));
+        }
+    }
+
+    private void Detonate()
+    {
+        Destroy(gameObject);
+    }
+}
