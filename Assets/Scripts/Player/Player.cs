6792c0a68dffae9780153d7a6b9321cddb61bc2d
ixavon1 <ixavsigler@gmail.com>
2025-02-25 19:07:04 -0500

    arrow key sypport

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -58,12 +58,12 @@ public class Player : MonoBehaviour
     private void Update()
     {
         // XZ input
-        if (Input.GetKey(KeyCode.A)) { input.x = -1; }
-        else if (Input.GetKey(KeyCode.D)) { input.x = 1; }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) { input.x = -1; }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) { input.x = 1; }
         else { input.x = 0; }
 
-        if (Input.GetKey(KeyCode.W)) { input.y = 1; }
-        else if (Input.GetKey(KeyCode.S)) { input.y = -1; }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) { input.y = 1; }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) { input.y = -1; }
         else { input.y = 0; }
 
         if (input != Vector2.zero)
