40e22b6b2aee937f8b341764348fcc795bdfd93d
ixavon1 <ixavsigler@gmail.com>
2025-02-27 00:47:13 -0500

    who cares.

---
+++ Assets/Scripts/GroundTileSwitcher.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTileSwitcher : MonoBehaviour
+{
+    private void Start()
+    {
+        GameManager.inst +=
+    }
+}
