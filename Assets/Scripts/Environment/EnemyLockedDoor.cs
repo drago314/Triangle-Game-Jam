53cd4dec8f9e187a960d88bb4c7fb76d8730cbe0
Avrick Altmann <avrickaltmann@yahoo.com>
2025-02-26 21:40:19 -0500

    Enemy Locked Door

---
+++ Assets/Scripts/Environment/EnemyLockedDoor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLockedDoor : MonoBehaviour
+{
+    public List<GameObject> enemies;
+    public float downSpeed, yDown;
+
+    int count;
+    float yGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var enemy in enemies)
+        {
+            enemy.GetComponent<Health>().OnDeath += OnDeath;
+        }
+        count = enemies.Count;
+        yGoal = transform.position.y - yDown;
+    }
+
+    private void Update()
+    {
+        if (count == 0)
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, yGoal, downSpeed), transform.position.z);
+    }
+
+    void OnDeath()
+    {
+        count -= 1;
+    }
+}
