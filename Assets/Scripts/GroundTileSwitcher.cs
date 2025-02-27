6cc0eedf216512a1eee38dc87bb97d28cad3b5d5
ixavon1 <ixavsigler@gmail.com>
2025-02-27 01:11:16 -0500

    dimension swtiching

--- Assets/Scripts/GroundTileSwitcher.cs
+++ Assets/Scripts/GroundTileSwitcher.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class GroundTileSwitcher : MonoBehaviour
 {
+    public Texture2D[] textures;
+    public Material mat;
+    public TerrainLayer layer;
+
     private void Start()
     {
-        GameManager.inst +=
+        GameManager.Inst.OnDimensionSwitch += OnSwitch;
+        OnSwitch(0);
+    }
+
+    private void OnSwitch(Dimension dim)
+    {
+        if(mat) mat.mainTexture = textures[(int)dim];
+        if (layer) layer.diffuseTexture = textures[(int)dim];
     }
 }
