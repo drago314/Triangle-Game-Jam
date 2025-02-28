3158fbbe3f9c7e909649a6927c87f126878d6897
Vivek Chandy <vac23@duke.edu>
2025-02-28 00:09:49 -0500

    started duck game

---
+++ Assets/Scripts/DuckShooter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckShooter : MonoBehaviour
+{
+    public GameObject duck;
+    public Transform[] DuckSpawnPoints;
+    public Transform[] DuckEndPoints;
+    public float avgSpawnTime; // Same average spawn time for all spawners
+
+    void Start()
+    {
+        for (int i = 0; i < DuckSpawnPoints.Length; i++)
+        {
+            StartCoroutine(SpawnDucks(i));
+        }
+    }
+
+    IEnumerator SpawnDucks(int index)
+    {
+        while (true)
+        {
+            Transform spawnPoint = DuckSpawnPoints[index];
+            Transform endPoint = DuckEndPoints[index];
+            GameObject go = Instantiate(duck, spawnPoint.position, spawnPoint.rotation);
+            go.GetComponent<Duck>().startPoint = spawnPoint.position;
+            
+            float spawnInterval = Random.Range(avgSpawnTime * 0.5f, avgSpawnTime * 1.5f);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
