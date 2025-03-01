5620455e8f0862104667301760608964e157f292
ixavon1 <ixavsigler@gmail.com>
2025-03-01 01:45:24 -0500

    intro + random changes

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraShake : MonoBehaviour
 {
@@ -8,9 +9,27 @@ public class CameraShake : MonoBehaviour
     float currentShakeAmount;
     Vector3 startPos;
 
+    public bool introAnim;
+    public float lerpSpeed, standardFov;
+    float realLerpSpeed;
+    public Vector3 standardPos, standardRot;
+    bool chill;
+    public GameObject[] toEnable;
+    public Player player;
+
+    Camera cam;
+
     private void Start()
     {
         startPos = transform.localPosition;
+        if (introAnim) 
+        {
+            cam = GetComponent<Camera>();
+            startPos = standardPos; 
+            Invoke("Anim", 3);
+            Invoke("Chill", 5);
+            // if (PlayerPrefs.GetFloat("CheckpointX" + SceneManager.GetActiveScene().buildIndex) != 0) Chill();
+        }
     }
 
     private void Update()
@@ -22,10 +41,28 @@ public class CameraShake : MonoBehaviour
 
             transform.localPosition = startPos + new Vector3(Random.Range(-currentShakeAmount, currentShakeAmount), Random.Range(-currentShakeAmount, currentShakeAmount));
         }
+
+        if (introAnim && !chill)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, standardPos, realLerpSpeed * Time.deltaTime);
+            transform.localEulerAngles = new Vector3(Mathf.LerpAngle(transform.localEulerAngles.x, standardRot.x, realLerpSpeed * Time.deltaTime), Mathf.LerpAngle(transform.localEulerAngles.y, standardRot.y, realLerpSpeed * Time.deltaTime), Mathf.LerpAngle(transform.localEulerAngles.z, standardRot.z, realLerpSpeed * Time.deltaTime));
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, standardFov, realLerpSpeed * Time.deltaTime * 3);
+        }
     }
 
     public void Shake(float amount, float dissipation)
     {
         if (amount > currentShakeAmount) { currentShakeAmount = amount; shakeDissipation = dissipation; }
+    }
+
+    private void Anim() { realLerpSpeed = lerpSpeed; }
+    private void Chill() 
+    {
+        chill = true;
+        transform.localPosition = standardPos;
+        transform.localEulerAngles = standardRot;
+        cam.fieldOfView = standardFov;
+        player.startScreenPos = cam.WorldToScreenPoint(player.transform.position);
+        foreach (GameObject go in toEnable) { go.SetActive(true); }
     }
 }
