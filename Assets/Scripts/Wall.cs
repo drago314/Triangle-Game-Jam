bc8e9235045794cee5e6bab4733add6efef0e633
Avrick Altmann <avrickaltmann@yahoo.com>
2025-03-01 00:22:59 -0500

    Stuff

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,6 +6,7 @@ public class Wall : MonoBehaviour
 {
     public LayerMask ground;
     public bool doPrint;
+
     private void Awake()
     {
         Collider[] c = Physics.OverlapSphere(transform.position, 0.3f, ground);
