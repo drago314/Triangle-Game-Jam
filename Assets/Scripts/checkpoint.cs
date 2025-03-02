5f7e8e54a90b4dc409aa9a8a858b3b6e92c18f39
Avrick Altmann <avrickaltmann@yahoo.com>
2025-03-02 06:37:23 -0500

    Audio Checkpoints

--- Assets/Scripts/checkpoint.cs
+++ Assets/Scripts/checkpoint.cs
@@ -8,6 +8,11 @@ public class checkpoint : MonoBehaviour
     public MeshRenderer myMat;
     public Texture sprite1, sprite2;
     public ParticleSystem ps;
+    public AudioSource fun;
+    public AudioSource narrator;
+    public AudioClip clip;
+    private bool playedClip = false;
+
 
     private void Start()
     {
@@ -20,12 +25,14 @@ public class checkpoint : MonoBehaviour
             string thing = scene.buildIndex.ToString();
             PlayerPrefs.SetFloat("CheckpointX" + thing, transform.position.x);
             PlayerPrefs.SetFloat("CheckpointZ" + thing, transform.position.z);
-            GetComponent<AudioSource>().Play();
+            fun.Play();
             ps.Play();
             GameManager.Inst.player.health.Heal(100);
             GameManager.Inst.PushStatus("Checkpoint!");
 
             myMat.material.mainTexture = sprite2;
+
+            if (!playedClip && clip != null) { narrator.PlayOneShot(clip, 1f); playedClip = true; }
         }
     }
 }
