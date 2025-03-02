7201b5e3532b41299b80828034bdd7b2e92c9e36
Avrick Altmann <avrickaltmann@yahoo.com>
2025-03-01 19:38:06 -0500

    Conc

--- Assets/Scripts/SceneTransitioner.cs
+++ Assets/Scripts/SceneTransitioner.cs
@@ -8,10 +8,18 @@ public class SceneTransitioner : MonoBehaviour
     public string scene;
     public GameObject eyelid1, eyelid2;
 
+    private bool loaded = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.TryGetComponent<Player>(out Player _))
+        if (!other.TryGetComponent<Player>(out Player _) || loaded)
             return;
+        loaded = true;
+        NextScene();
+    }
+
+    public void NextScene()
+    {
         eyelid1.SetActive(true);
         eyelid2.SetActive(true);
 
