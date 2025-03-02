2d6d767d05176593c61d0e30ab4d3cdf2eb6a796
ixavon1 <ixavsigler@gmail.com>
2025-03-02 02:12:18 -0500

    pause menu

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -6,24 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    public int defaultScene = 1;
-    public Slider slider;
-    public void OnClickStart(){
-        int currentScene = PlayerPrefs.GetInt("Scene");
-        Debug.Log(currentScene);
-        if(currentScene == 0)
-            SceneManager.LoadScene(defaultScene);
-        else
-            SceneManager.LoadScene(currentScene);
-    }
-
-    public void OnQuit(){
-        Application.Quit();
-    }
-
-    public void audioListener(){
-        PlayerPrefs.SetFloat("GameVolume",slider.value);
-        AudioListener.volume = slider.value;
-        Debug.Log("value set to " + slider.value);
-    }
+    public void Unpause() { GameManager.Inst.TogglePause(); }
+    public void Quit() { Application.Quit(); }
 }
